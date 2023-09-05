abb1e86d3e3e993691aa4f96256998beb014ceca
Samuel Corrales <sacosa634@gmail.com>
2023-09-05 21:06:53 +0000

    Commit inicial

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include "mbed.h"
 #include <iostream>
-#define BLINKING_RATE     500ms
+using namespace std;
+    
+PwmOut ledRojo(LED1);
+PwmOut ledVerde(LED2);
+PwmOut ledAzul(LED3);
 
 int main() {
 
-    PwmOut ledRojo(LED1);
-    PwmOut ledVerde(LED2);
-    PwmOut ledAzul(LED3);
+    string hex_code;
 
-    float valorR, valorG, valorB;
+    cout << "Enter an RGB hexadecimal color code: ";
+    cin >> hex_code;
 
-    cout << "Ingrese el valor para R: ";
-    cin << valorR;
+    // Remove the '#' character from the beginning of the hex code.
 
-    cout << "Ingrese el valor para G: ";
-    cin << valorG;
+    std::string hex_code_no_hash(hex_code);
+    hex_code_no_hash.erase(0, 1);
 
-    cout << "Ingrese el valor para B: ";
-    cin << valorB;
+    // Convert each of the two digits of the hex code to an integer.
 
-    ledRojo.write(valorR);
-    ledVerde.write(valorG);
-    ledAzul.write(valorB);
+    int red = std::stoi(hex_code_no_hash.substr(0, 1), nullptr, 16);
+    int green = std::stoi(hex_code_no_hash.substr(2, 3), nullptr, 16);
+    int blue = std::stoi(hex_code_no_hash.substr(4, 5), nullptr, 16);
 
+    ledRojo = red;
+    ledVerde = green;
+    ledAzul = blue;
 }
